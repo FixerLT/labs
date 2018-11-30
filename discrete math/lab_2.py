373ff5a9ca79760e1705a3800b9ed8e274f08074
Mark Barho <zazazasaza@mail.ru>
2018-11-30 16:38:11 +0200

    Update lab_2.py

--- discrete math/lab_2.py
+++ discrete math/lab_2.py
@@ -27,3 +27,50 @@ for i in range(50):
         else:
             connections[i].append(0)
 print_graph(names, connections)
+
+def get_table(names1, names2, connections):
+    tab = max([len(e) for e in names1]) + 3
+    shift = 3
+    starts = []
+    ends = []
+    sm = tab+shift
+    for e in names2:
+        starts.append(sm)
+        sm += len(e)
+        ends.append(sm)
+        sm += shift
+    positions = []
+    for start, end in zip(starts, ends):
+        positions.append(int((start + end)/2))
+    sm -= shift
+    result = ' ' * tab
+    for e in names2:
+        result += ' ' * shift
+        result += e
+    for e, i in zip(names1, range(len(names1))):
+        result += '\n'
+        result += e + ' ' * (tab - len(e))
+        for e2, j in zip(names2, range(len(names2))):
+            result += ' ' * shift
+            pos = int((len(e2)+1) / 2)
+            result += ' ' * (pos - 1)
+            result += '1' if connections[i][j] else '0'
+            result += ' ' * (len(e2) - pos) if pos!=0 else ''
+    # for i in range(len(names1)):
+    #     for j in range(len(names2)):
+    #         result[(sm+1) * (j+1) + positions[i]] = '1' if connections[i][j] else '0'
+    return result
+
+n1 = ['asdf', 'aa', 'asdf3', 'a', '228']
+n2 = ['123', '11', '1', '134134', '555', '12341234']
+names1 = [str(e) for e in range(5)]
+names2 = [str(e) for e in range(6)]
+connections = []
+for e in range(5):
+    connections.append([])
+    for k in range(6):
+        connections[e].append((e+k)%3 == 1)
+print(get_table(n1, n2, connections))
+print(get_table(names1, names2, connections))
+print('a' * 0)
+print('hui')
