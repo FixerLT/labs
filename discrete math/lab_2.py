d7a37ba4710a2d543a3a793e545b407a5cb188d4
Mark Barho <mark.barho@gmail.com>
2018-12-12 17:42:40 +0200

    Update lab_2.py

--- discrete math/lab_2.py
+++ discrete math/lab_2.py
@@ -1,7 +1,6 @@
 import random
 import igraph
 from igraph import *
-print(igraph.__version__)
 
 DEBUG1 = False
 DEBUG2 = False
@@ -9,7 +8,8 @@ DEBUG3 = False
 
 u_to_ind = {}
 
-def print_graph(names, connections, folder = '/home/san/Documents/university/babakov/lab2/testing/', filename = 'test0'):
+
+def print_graph(names, connections, folder='/home/san/Documents/university/babakov/lab2/testing/', filename='test0'):
     g = Graph().as_directed()
     g.add_vertices(len(names))
     # g.vs['label'] = names
@@ -19,8 +19,12 @@ def print_graph(names, connections, folder = '/home/san/Documents/university/bab
                 g.add_edges([(i, j)])
     arrow_size = [1 for e in g. get_edgelist()]
     # print(arrow_size)
-    plot(g, folder+filename+'.pdf', layout=g.layout('fr'), vertex_label=[str(u_to_ind[e]) for e in names], edge_arrow_size=arrow_size, arrow_size=arrow_size, edge_arrow_width=arrow_size)
-def print_graph2(names1, names2, connections, folder = '/home/san/Documents/university/babakov/lab2/testing/', filename = 'test0'):
+    plot(g, folder+filename+'.png', layout=g.layout('fr'), vertex_label=[str(u_to_ind[e]) for e in names],
+         edge_arrow_size=arrow_size, arrow_size=arrow_size, edge_arrow_width=arrow_size)
+
+
+def print_graph2(names1, names2, connections,
+                 folder='/home/san/Documents/university/babakov/lab2/testing/', filename='test0'):
     g = Graph().as_directed()
     g.add_vertices(len(names1) + len(names2))
     # g.vs['label'] = names
@@ -30,7 +34,9 @@ def print_graph2(names1, names2, connections, folder = '/home/san/Documents/univ
                 g.add_edges([(i, j + len(names1))])
     arrow_size = [1 for e in g. get_edgelist()]
     # print(arrow_size)
-    plot(g, folder+filename+'.pdf', layout=g.layout('fr'), vertex_label=[str(u_to_ind[e]) for e in names1+names2], edge_arrow_size=arrow_size, arrow_size=arrow_size, edge_arrow_width=arrow_size)
+    plot(g, folder+filename+'.png', layout=g.layout('fr'), vertex_label=[str(u_to_ind[e]) for e in names1+names2],
+         edge_arrow_size=arrow_size, arrow_size=arrow_size, edge_arrow_width=arrow_size)
+
 
 if DEBUG1:
     print_graph(['a', 'b', 'c'], [[0, 1, 0], [1, 0, 0], [0, 0, 0]])
@@ -40,19 +46,20 @@ if DEBUG1:
     for e in range(5):
         connections.append([])
         for k in range(6):
-            if(e+k < 8):
+            if e+k < 8:
                 connections[e].append(True)
             else:
                 connections[e].append(False)
     print_graph2(t1, t2, connections)
 
-def get_table(names1, names2, connections):
-    tab = max([len(e) for e in names1]) + 3
+
+def get_table(rows_names, connections, table_signs):
+    tab = max([len(e) for e in rows_names]) + 3
     shift = 3
     starts = []
     ends = []
     sm = tab+shift
-    for e in names2:
+    for e in connections:
         starts.append(sm)
         sm += len(e)
         ends.append(sm)
@@ -62,22 +69,20 @@ def get_table(names1, names2, connections):
         positions.append(int((start + end)/2))
     sm -= shift
     result = ' ' * tab
-    for e in names2:
+    for e in connections:
         result += ' ' * shift
         result += e
-    for e, i in zip(names1, range(len(names1))):
+    for i, e in enumerate(rows_names):
         result += '\n'
         result += e + ' ' * (tab - len(e))
-        for e2, j in zip(names2, range(len(names2))):
+        for e2, j in zip(connections, range(len(connections))):
             result += ' ' * shift
             pos = int((len(e2)+1) / 2)
             result += ' ' * (pos - 1)
-            result += '1' if connections[i][j] else '0'
-            result += ' ' * (len(e2) - pos) if pos!=0 else ''
-    # for i in range(len(names1)):
-    #     for j in range(len(names2)):
-    #         result[(sm+1) * (j+1) + positions[i]] = '1' if connections[i][j] else '0'
+            result += '1' if table_signs[i][j] else '0'
+            result += ' ' * (len(e2) - pos) if pos != 0 else ''
     return result
+
 
 def get_pairs(names1, names2, connections):
     pairs = []
@@ -87,7 +92,8 @@ def get_pairs(names1, names2, connections):
                 pairs.append((names1[i], names2[j]))
     return pairs
 
-def save_to_file2(names1, names2, connections, description, folder_name = 'testing', pref='', singular=False):
+
+def save_to_file2(names1, names2, connections, description, folder_name='testing', pref='', singular=False):
     path = '/home/san/Documents/university/babakov/lab2/' + folder_name + '/'
     if singular:
         print_graph(names1, connections, path, pref+'graph')
@@ -95,7 +101,7 @@ def save_to_file2(names1, names2, connections, description, folder_name = 'testi
         print_graph2(names1, names2, connections, path, pref + 'graph')
     pairs = get_pairs(names1, names2, connections)
     with open(path + pref + 'pairs.txt', 'w+') as f:
-        f.write(str(pairs))
+        f.write(str(pairs).replace("'", ''))
     table = get_table(names1, names2, connections)
     with open(path + pref + 'table.txt', 'w+') as f:
         f.write(table)
@@ -111,7 +117,7 @@ if DEBUG2:
     for e in range(5):
         connections.append([])
         for k in range(6):
-            if(e+k < 8):
+            if e+k < 8:
                 connections[e].append(True)
             else:
                 connections[e].append(False)
@@ -119,24 +125,23 @@ if DEBUG2:
 
 vowels = set()
 a = ord('а')
-# z1 = ord('я')+1
 z1 = ord('а')+11
-hui = 0
-while a<z1:
+while a < z1:
     vowels.add(chr(a))
     vowels.add(chr(ord('я') - (a - ord('а'))))
     a += 1
 print(vowels)
 
+
 def str_to_int(name):
     name = name.replace(' ', '').replace('-', '').lower()
-    # vowels = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н'}
-    val = 0
+    str_value = 0
     for e in name:
-        val *= 2
+        str_value *= 2
         if e in vowels:
-            val += 1
-    return val
+            str_value += 1
+    return str_value
+
 
 a = ['Велосипед', 'Самокат', 'Роликовые коньки', 'Уницикл', 'Скейтборд', 'Галера', 'Каяка', 'Катамаран']
 b = ['Трамвай', 'Троллейбус', 'Поезд', 'Электричка', 'Самолёт', 'Баржа', 'Маршутное такси', 'Автобус']
@@ -164,6 +169,9 @@ for i, e in enumerate(u):
 for i in range(len(u)):
     if u_int[i] != str_to_int(u[i]):
         print('hui')
+for i, e in enumerate(u):
+    print(str(i) + '\t' + str(e) + '\t\t' + str(str_to_int(e)))
+print(u)
 a_int = [str_to_int(e) for e in a]
 b_int = [str_to_int(e) for e in b]
 c_int = [str_to_int(e) for e in c]
@@ -180,14 +188,13 @@ if DEBUG3:
     print(sum([1 for e in cnt.values() if e > 1]))
     bad_keys = []
     for k, v in cnt.items():
-        if v>1:
+        if v > 1:
             bad_keys.append(k)
     print(bad_keys)
     bad_names = []
     for e in u:
-        if(str_to_int(e) in bad_keys):
+        if str_to_int(e) in bad_keys:
             bad_names.append(e)
-    # bad_names = [e for e in u if (str_to_int(e) in bad_keys)]
     print(bad_names)
 
 
@@ -202,30 +209,28 @@ def create_connections(ar1, ar2, my_fn):
                 res[-1].append(False)
     return res
 
+
 def create_connections_arr(ar1, ar2, my_fn):
     res = []
     for e1 in ar1:
         res.append(my_fn(e1, ar2))
     return res
 
+
 description1 = "конгруентный элемент по модулю 7"
-fn = lambda x, y: x % 7 == y % 7
-connections = create_connections(a_int, b_int, fn)
+connections = create_connections(a_int, b_int, lambda x, y: x % 7 == y % 7)
 save_to_file2(a, b, connections, description1, '1')
 
 description2 = "сумма конгруэнтна 5 по модулю 8"
-fn = lambda x, y: (x+y) %8 == 5
-connections = create_connections(a_int, c_int, fn)
+connections = create_connections(a_int, c_int, lambda x, y: (x+y) % 8 == 5)
 save_to_file2(a, c, connections, description2, '2')
 
 description3 = "разность конгруентна 2 по модулю 3"
-fn = lambda x, y: ((x%3) - (y%3) + 3) % 3 == 2
-connections = create_connections(b_int, d_int, fn)
+connections = create_connections(b_int, d_int, lambda x, y: ((x%3) - (y%3) + 3) % 3 == 2)
 save_to_file2(b, d, connections, description3, '3')
 
 description4 = "квадрат первого без значения второго кратен 6"
-fn = lambda x, y: abs(x*x - y) % 6 == 0
-connections = create_connections(c_int, b_int, fn)
+connections = create_connections(c_int, b_int, lambda x, y: abs(x*x - y) % 6 == 0)
 save_to_file2(c, b, connections, description4, '4')
 
 description5 = "ближайший элемент"
@@ -238,13 +243,7 @@ connections = create_connections_arr(c_int, d_int, fn)
 save_to_file2(c, d, connections, description5, '5', pref='cd.')
 
 description6 = "минимальный ближайший элемент"
-# fn = lambda x, ar: [True if (abs(x-e) == min([abs(x-k) for k in ar])) else False for e in ar]
 fn = lambda x, ar: [True if (l == min([k for k in ar if (abs(x-k) == min([abs(x-e) for e in ar]))])) else False for l in ar]
-# def create_6_connections(ar1, ar2):
-#     name_to_index1 = {}
-#     name_to_index2 = {}
-#     for i in range(len(ar1)):
-
 connections = create_connections_arr(a_int, b_int, fn)
 save_to_file2(a, b, connections, description6, '6', pref='ab.')
 connections = create_connections_arr(b_int, c_int, fn)
@@ -252,11 +251,12 @@ save_to_file2(b, c, connections, description6, '6', pref='bc.')
 connections = create_connections_arr(c_int, d_int, fn)
 save_to_file2(c, d, connections, description6, '6', pref='cd.')
 
+
 def get_sorted_connections_to_next(my_ar):
     good = {}
     sorted_ar = sorted(my_ar)
     for i in range(len(my_ar)):
-        good[sorted_ar[i]] = sorted_ar[(i+1)%len(my_ar)]
+        good[sorted_ar[i]] = sorted_ar[(i+1) % len(my_ar)]
     res = []
     for e in my_ar:
         res.append([])
@@ -267,30 +267,33 @@ def get_sorted_connections_to_next(my_ar):
                 res[-1].append(False)
     return res
 
+
 description71 = "xQx для любого x из заданного множества"
 description72 = "следующий элемент в отсортированном списке"
-all_true_fn = lambda x, y:True
-connections = create_connections(a_int, a_int, all_true_fn)
+connections = create_connections(a_int, a_int, lambda x, y: True)
 save_to_file2(a, a, connections, description71, '7', pref='all.', singular=True)
 connections = get_sorted_connections_to_next(a_int)
 save_to_file2(a, a, connections, description72, '7', pref='next.', singular=True)
 
-connections = create_connections(b_int, b_int, all_true_fn)
+connections = create_connections(b_int, b_int, lambda x, y: True)
 save_to_file2(b, b, connections, description71, '8', pref='all.', singular=True)
 connections = get_sorted_connections_to_next(b_int)
 save_to_file2(b, b, connections, description72, '8', pref='next.', singular=True)
 
-connections = create_connections(c_int, c_int, all_true_fn)
+connections = create_connections(c_int, c_int, lambda x, y: True)
 save_to_file2(c, c, connections, description71, '9', pref='all.', singular=True)
 connections = get_sorted_connections_to_next(c_int)
 save_to_file2(c, c, connections, description72, '9', pref='next.', singular=True)
 
-connections = create_connections(d_int, d_int, all_true_fn)
+connections = create_connections(d_int, d_int, lambda x, y: True)
 save_to_file2(d, d, connections, description71, '10', pref='all.', singular=True)
 connections = get_sorted_connections_to_next(d_int)
 save_to_file2(d, d, connections, description72, '10', pref='next.', singular=True)
 
-connections = create_connections(u_int, u_int, all_true_fn)
+connections = create_connections(u_int, u_int, lambda x, y: True)
+# range_str = [str(e) for e in range(len(u))]
 save_to_file2(u, u, connections, description71, '11', pref='all.', singular=True)
+# save_to_file2(range_str, range_str, connections, description71, '11', pref='all.', singular=True)
 connections = get_sorted_connections_to_next(u_int)
 save_to_file2(u, u, connections, description72, '11', pref='next.', singular=True)
+# save_to_file2(range_str, range_str, connections, description72, '11', pref='next.', singular=True)
