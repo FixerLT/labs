43e5f330a4efc16d1968a84c0fcad946396c31fd
Mark Barho <zazazasaza@mail.ru>
2018-12-16 14:00:04 +0200

    code quality improve

--- discrete math/helpers/generate_lab58_graph.py
+++ discrete math/helpers/generate_lab58_graph.py
@@ -5,9 +5,8 @@ def get_distance_between_pubs(name1, name2):
     if name1 == name2:
         return [len(name1)]
     distances = set()
-    if name1.find('Bar') != -1 and name2.find('Bar') != -1:
-        distances.add(1)
-    if name1.find('Pub') != -1 and name2.find('Pub') != -1:
+    if ('bar' in name1.lower() and 'bar' in name2.lower()) or \
+            ('pub' in name1.lower() and 'pub' in name2.lower()):
         distances.add(1)
     if len(name1) == len(name2):
         distances.add(len(name1))
@@ -15,11 +14,10 @@ def get_distance_between_pubs(name1, name2):
         distances.add((len(name2) - len(name1)) * 2)
     else:
         distances.add(len(name1) - len(name2))
-    distances = [e for e in distances]
-    return distances
+    return list(distances)
 
 
-def get_graph_for_topic_1():
+def get_graph_for_topic_1(test_save=None):
     nodes = ['Signal Bar', 'The Drinkery', 'Barrel Pub', 'BarDuck', 'Varvar Bar',
              'Beer.Live', 'Mugnum94', 'Pravda Beer Theatre', 'OLD BAR', 'Pilsner Bar',
              'Black Bull Bar', 'MumuBar', 'CRAFT vs PUB', 'Steam Pub', 'Porter Pub']
@@ -29,7 +27,12 @@ def get_graph_for_topic_1():
         for j in range(len(nodes)):
             distances[-1].append(get_distance_between_pubs(nodes[i], nodes[j]))
     graph = LabGraph(nodes, distances)
-    graph.save_plot('/home/san/Documents/university/babakov/lab5/test/', 'topic1_source')
-    return LabGraph(nodes, distances)
+    if test_save is not None:
+        graph.save_plot(test_save[0], test_save[1])
+    return graph
 
-get_graph_for_topic_1()
+
+if __name__ == "__main__":
+    get_graph_for_topic_1(('/home/san/Documents/university/babakov/lab5/test/', 'topic1_source'))
+
+
