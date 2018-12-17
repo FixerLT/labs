ea0de0e6f4d92d8306e1022305d3d0e913e18258
Mark Barho <zazazasaza@mail.ru>
2018-12-17 18:08:31 +0200

    code reviewed for lab5

--- discrete math/lab_5.py
+++ discrete math/lab_5.py
@@ -3,9 +3,13 @@ from generate_lab58_graph import get_graph_for_topic_not_1
 
 def save_to_file(report, path, file_name):
     print(report)
-    return
+    return # TODO remove this debug statement fo really save reports
     with open(path + file_name + '.txt') as f:
         f.write(report)
+
+
+def apply_str_stub_to_array(str_stub, arr, step='\n'):
+    return ''.join(str_stub.format(*e) + step for e in arr)
 
 
 graph = get_graph_for_topic_not_1()
@@ -16,104 +20,55 @@ task1 = task1.format(str(graph.nodes), len(graph.nodes), graph.edges_list, len(g
 save_to_file(task1, path, '1')
 
 
-adjacent_nodes = []
-for i in range(len(graph.nodes)):
-    adjacent_nodes.append(set())
-    for j in range(len(graph.nodes)):
-        if len(graph.edges[i][j]) > 0:
-            adjacent_nodes[-1].add(j)
-        if len(graph.edges[j][i]) > 0:
-            adjacent_nodes[-1].add(j)
+adjacent_nodes = [len(graph.get_neighbours(i)) for i in range(len(graph.nodes))]  # количество соседей для каждой i
+max_neighbours = max(adjacent_nodes)
+nodes_with_max_neighbours = list(filter((lambda e: e[1] == max_neighbours), enumerate(adjacent_nodes)))
 
-max_ = 0
-max_index = []
-for i in range(len(graph.nodes)):
-    if len(adjacent_nodes[i]) == max_:
-        max_index.append(i)
-    if len(adjacent_nodes[i]) > max_:
-        max_ = len(adjacent_nodes[i])
-        max_index = [i]
-
-task2 = 'Вершина №{} {} смежных вершин\n\n' # TODO if two or more nodes have max amount of adjacented nodes
-task2 = task2.format(str(max_index), str(max_))
+task2 = 'Вершина №{} {} смежных вершин'
+task2 = apply_str_stub_to_array(task2, nodes_with_max_neighbours)
+task2 += '\n'
 save_to_file(task2, path, '2')
 
+degrees = graph.get_node_degrees(orientated=True)
+max_degree, min_degree = max(degrees), min(degrees)
+max_degree_nodes = [i for i, e in enumerate(degrees) if e == max_degree]
+min_degree_nodes = [i for i, e in enumerate(degrees) if e == min_degree]
+task3_max = 'Вершина №{} имеет максимальную степень {}'
+task3_min = 'Вершина №{} имеет минимальную степень {}'
+task3_max = apply_str_stub_to_array(task3_max, [(e, max_degree) for e in max_degree_nodes])
+task3_min = apply_str_stub_to_array(task3_min, [(e, min_degree) for e in min_degree_nodes])
+save_to_file(task3_min + '\n' + task3_max + '\n', path, '3')
 
-max_index = []
-max_ = 0
-min_index = []
-min_ = 0
-isolated_vertex = []
-pendant_vertex = []
-
-for i in range(len(graph.nodes)):
-    curr_edges = 0
-    for j in adjacent_nodes[i]:
-        curr_edges += len(graph.edges[i][j])+len(graph.edges[j][i])
-        if i == j:
-            curr_edges -= len(graph.edges[i][j])
-    if i == 0:
-        min_ = curr_edges
-    if curr_edges == max_:
-        max_index.append([i])
-    if curr_edges == min_:
-        min_index.append([i])
-    if curr_edges > max_:
-        max_ = curr_edges
-        max_index = [i]
-    if curr_edges < min_:
-        min_ = curr_edges
-        min_index = [i]
-    if curr_edges == 0:
-        isolated_vertex.append(i)
-    if curr_edges == 1:
-        pendant_vertex.append(i)
-
-task3 = 'Вершина №{} имеет максимальную степень {}\nВершина №{} имеет минимальную степень {}\n\n' # TODO two or more nodes have max/min degree
-task3 = task3.format(str(max_index), str(max_), str(min_index), str(min_))
-save_to_file(task3, path, '3')
-
+isoted_nodes = [i for i, e in enumerate(degrees) if e == 0]
+pendant_nodes = [i for i, e in enumerate(degrees) if e == 1]
 task4 = 'Количество изолированных вершин: {}\nКоличество концевых вершин: {}\n\n'
-task4 = task4.format(str(len(isolated_vertex)), str(len(pendant_vertex)))
+task4 = task4.format(str(len(isoted_nodes)), str(len(pendant_nodes)))
 save_to_file(task4, path, '4')
 
-edges_list = graph.edges_list.copy()
-edges_list.sort()
-max_ = 0
-max_index = 0
-curr_edges = 0
-for i in range(1, len(edges_list)):
-    if edges_list[i][0] == edges_list[i-1][0] and edges_list[i][1] == edges_list[i-1][1]:
-        curr_edges += 1
-    else:
-        if curr_edges > max_:
-            max_ = curr_edges
-            max_index = i-1
-        curr_edges = 0
-if curr_edges > max_:
-    max_ = curr_edges
-    max_index = i-1
-max_ += 1
-
-task5 = 'Ребро {} -> {} имеет максимальную кратность({})\n\n'# TODO two or more edges have max multiplicity
-task5 = task5.format(str(edges_list[max_index][0]), str(edges_list[max_index][1]), max_)
+max_degree = max([max([len(e) for e in row if e is not None]) for row in graph.edges])
+max_degree_edges_arrays = ([[(i, j, max_degree) for i in range(len(graph.nodes)) if graph.edges[i][j] is not None and len(graph.edges[i][j]) == max_degree] for j in range(len(graph.nodes))])
+max_degree_edges = []
+for e in max_degree_edges_arrays:
+    max_degree_edges.extend(e)
+task5 = 'Ребро {} -> {} имеет максимальную кратность({})'# TODO two or more edges have max multiplicity
+task5 = apply_str_stub_to_array(task5, max_degree_edges) + '\n'
 save_to_file(task5, path, '5')
-max_multiplicity = max_
 
-loop_count = 0
-for i in range(len(graph.nodes)):
-    loop_count += len(graph.edges[i][i])
+loop_count = len([i for i in range(len(graph.nodes)) if graph.edges[i][i] is not None and len(graph.edges[i][i]) > 0])
 task6 = 'Количество петель: {}\n\n'
 task6 = task6.format(str(loop_count))
 save_to_file(task6, path, '6')
 
 #простым граф не может быть простым по условию (см. Лаб №0 п. 3)
 # давай делать скрипт lab_5 независимо от условия в 0 лабе второго семестра. Чтобы наш код определял тип для всех случаев
-s=""
-if loop_count == 0:
-    s = "мультиграфом"
+
+if max_degree > 1:
+    if loop_count > 0:
+        s = "псевдографом"
+    else:
+        s = "мультиграфом"
 else:
-    s = "псевдографом"
+    s = 'простым'
 task7 = 'Граф является {}\n\n'
 task7 = task7.format(s)
 save_to_file(task7, path, '7')
